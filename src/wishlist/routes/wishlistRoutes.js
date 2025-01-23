34b5a20f1d084a98f9a0bcf94ad1789c3a0ed43a
jsm1129 <jamja0513@sookmyung.ac.kr>
2025-01-23 21:54:35 +0900

    위시리스트 코드 수정정

--- src/wishlist/routes/wishlistRoutes.js
+++ src/wishlist/routes/wishlistRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const wishlistController = require('../controllers/wishlistController');
 const authenticate = require('../../auth/middleware/authenticate');
 const router = express.Router();
-const upload = require('../../../config/multer');
+const { upload } = require('../../auth/utils/upload');
 
 // 선물 추가
 router.post('/', authenticate, upload.single('image'), wishlistController.addWishlist);
@@ -13,13 +13,16 @@ router.patch('/:gift_id', authenticate, upload.single('image'), wishlistControll
 // 선물 삭제
 router.delete('/:gift_id', wishlistController.deleteWishlist);
 
-//특정 선물 조회
-router.get('/:gift_id', wishlistController.getWishlistById);
+// 특정 선물 조회-생일자
+router.get('/', authenticate, wishlistController.getWishlistByMember);
+
+// 특정 선물 조회-선물 주는 사람
+router.get('/:gift_id', wishlistController.getWishlistByGiver);
 
 // 위시리스트 조회-생일자
 router.get('/member/birthday', authenticate, wishlistController.getWishlistByBirthday);
 
-//위시리스트 조회-선물 주는 사람
+// 위시리스트 조회-선물 주는 사람
 router.get('/giver/:member_id', wishlistController.getWishlistByMemberId);
 
 module.exports = router;
